2813888495ddcdfb3035d63f1d858d9bbbe44fcd
clertonf <clerton.filho7@gmail.com>
2021-09-08 04:33:33 -0300

    jest added and some tests accomplished

---
+++ src/__tests__/screens/profile.spec.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import { Profile } from "../../screens/Profile";
+
+describe("Profile Screen:", () => {
+  it("should have placeholder correctly input user name", () => {
+    const { getByPlaceholderText } = render(<Profile />);
+
+    const inputName = getByPlaceholderText("Nome");
+
+    expect(inputName).toBeTruthy();
+
+    //debug(); emulator
+  });
+
+  it("should be load user data", () => {
+    const { getByTestId } = render(<Profile />);
+
+    const inputName = getByTestId("input-name");
+    const inputSurname = getByTestId("input-surname");
+
+    expect(inputName.props.value).toEqual("Clerton");
+    expect(inputSurname.props.value).toEqual("Filho");
+  });
+
+  it("should exist title correctly", () => {
+    const { getByTestId } = render(<Profile />);
+
+    const textTitle = getByTestId("text-title");
+
+    expect(textTitle.props.children).toContain("Perfil"); //Filho do elemento
+  });
+});
